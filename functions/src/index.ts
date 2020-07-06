70c32fb9053bd6ee4a8d4b8e0119cb4029dedbb2
William Koller <wkoller25@gmail.com>
2020-07-06 18:53:47 -0300

    feat: add express

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,12 +1,5 @@
 import * as functions from 'firebase-functions'
 
-// Start writing Firebase Functions
-// https://firebase.google.com/docs/functions/typescript
-
-export const helloWorld = functions.https.onRequest((request, response) => {
-	response.send('Hello William')
-})
-
 export const statusServer = functions.https.onRequest((request, response) => {
 	response.send('Server online on port 5000')
 })
