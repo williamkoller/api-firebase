7a6e88b0be9d67beb4799e845fa3ccfb4be933db
William Koller <wkoller25@gmail.com>
2020-07-06 18:33:24 -0300

    feat: add new remote

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -6,3 +6,7 @@ import * as functions from 'firebase-functions'
 export const helloWorld = functions.https.onRequest((request, response) => {
 	response.send('Hello William')
 })
+
+export const statusServer = functions.https.onRequest((request, response) => {
+	response.send('Server online on port 5000')
+})
